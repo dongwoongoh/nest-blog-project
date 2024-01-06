115f56b9b9f961736364c54d71f0c059da63b7ba
maaaaaaaaad <integral5137@gmail.com>
2024-01-07 04:06:26 +0900

    e

--- google_call.sh
+++ google_call.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
-echo "search_query:"
+echo "search:"
 read search_query
 
 encoded_query=$(python -c "import urllib.parse; print(urllib.parse.quote(input()))" <<<"$search_query")
+
+echo "Encoded query: $encoded_query"
 
 open -a "Google Chrome" "https://www.google.com/search?q=$encoded_query"
